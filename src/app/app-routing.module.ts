7841da45e4c867fbfb61422bcd000923c435beba
Will <doggy.huang@gmail.com>
2020-03-07 14:15:20 +0800

    示範 onSameUrlNavigation: 'reload' 寫法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    onSameUrlNavigation: 'reload'
   })],
   exports: [RouterModule]
 })
