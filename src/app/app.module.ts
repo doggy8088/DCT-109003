e7d73aa732fe9606b3ac80bf2f22c605cface2e7
Will <doggy.huang@gmail.com>
2020-03-07 11:47:42 +0800

    示範 /tables, /charts, /utilities/colors, NotFoundComponent 元件建立與路由設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TablesComponent } from './tables/tables.component';
+import { ChartsComponent } from './charts/charts.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
     Page1Component,
     Page2Component,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    TablesComponent,
+    ChartsComponent,
+    ColorsComponent
   ],
   imports: [
     BrowserModule,
