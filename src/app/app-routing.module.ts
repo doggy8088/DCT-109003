fa27824abff35223c885cf60e63f99b5afd3cb1d
Will <doggy.huang@gmail.com>
2020-03-07 11:25:37 +0800

    示範萬用路由語法與 NotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
   { path: '', component: DashboardComponent },
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: 'page2', component: Page2Component },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
