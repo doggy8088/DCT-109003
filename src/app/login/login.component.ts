00a010b812988b2c17002da063127042c98b1ff0
Will <doggy.huang@gmail.com>
2020-03-14 11:17:42 +0800

    將表單加入 two-way binding 語法

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
+  data: any = {};
+
   origBodyClass = '';
 
   constructor() { }
