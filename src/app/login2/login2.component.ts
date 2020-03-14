c0282462f2e9d1c43dfbc0f2260abeb17d29a4ba
Will <doggy.huang@gmail.com>
2020-03-14 15:08:25 +0800

    示範用 FormBuilder 建立表單模型並套用至範本

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login2',
@@ -9,11 +10,30 @@ export class Login2Component implements OnInit, OnDestroy {
 
   origBodyClass = '';
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.origBodyClass = document.body.className;
     document.body.className = 'bg-gradient-primary';
+
+    // this.form = this.fb.group({
+    //   email: 'doggy.huang@gmail.com',
+    //   password: '123123123'
+    // });
+
+    this.form = this.fb.group({
+      email: ['doggy.huang@gmail.com'],
+      password: ['123123123'],
+      isRemember: [true],
+    });
+
+    // this.form = this.fb.group({
+    //   email: this.fb.control('doggy.huang@gmail.com'),
+    //   password: this.fb.control('123123123')
+    // });
+
   }
 
   ngOnDestroy(): void {
